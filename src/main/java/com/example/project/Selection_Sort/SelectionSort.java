35643b2c0c605e1cdbe05ce3e75717e3b05e1864
Jarian Martinez <jarianm2@nycstudents.net>
2025-02-25 01:20:37 +0000

    sorted

--- src/main/java/com/example/project/Selection_Sort/SelectionSort.java
+++ src/main/java/com/example/project/Selection_Sort/SelectionSort.java
@@ -8,35 +8,34 @@ public class SelectionSort {
 
     // PART A. implementing selection sort
     public static int[] selectionSort(int[] elements) {
-        for (int j = 0; j < elements.length - 1; j++) {
-            int minIndex = j;
-            for (int k = j + 1; k < elements.length; k++) {
-                if (elements[k] < elements[minIndex]) {
-                    minIndex = k;
+        for (int i = 0; i < elements.length - 1; i++) {
+            int min = i;
+            for(int j = i + 1; j < elements.length; j++) {
+                if(elements[j] < elements[min]) {
+                    min = j;
                 }
             }
-            int temp = elements[j];
-            elements[j] = elements[minIndex];
-            elements[minIndex] = temp;
+            int temp = elements[i];
+            elements[i] = elements[min];
+            elements[min] = temp;
         }
-
         return elements;
     }
 
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> selectionSortWordList(ArrayList<String> words) {
-        for (int j = 0; j < words.size() - 1; j++) {
-            int minIndex = j;
-            for (int k = j + 1; k < words.size(); k++) {
-                if (words.get(k).compareTo(words.get(minIndex)) < 0) {
-                    minIndex = k;
+        for (int i = 0; i < words.size() - 1; i++) {
+            int min = i;
+            for(int j = i + 1; j < words.size(); j++) {
+                int comparison = words.get(j).compareTo(words.get(min));
+                if( comparison < 0) {
+                    min = j;
                 }
             }
-            String temp = words.get(j);
-            words.set(j, words.get(minIndex));
-            words.set(minIndex, temp);
+            words.set(i, words.set(min, words.get(i)));
         }
+
         return words;
     }
 
@@ -51,6 +50,22 @@ public class SelectionSort {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
         return wordList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> words = SelectionSort.loadWordsInto(new ArrayList<String>());
+        System.out.println(words);
+        words = SelectionSort.selectionSortWordList(words);
+        System.out.println();
+        System.out.println(words);
+        boolean pass = true;
+        for(int i=1; i < words.size();i++){
+            if(words.get(i).compareTo(words.get(i-1))<0){ //not in alphabetical order
+                pass = false; 
+            }
+        }
+        System.out.println(pass);
     }
 }
